c7ba72d677c5bc4c42c44c9f7d389f344c29f405
VicvekSr <Olamideoso65@gmail.com>
2023-02-07 20:13:13 +0000

    add requests validator

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
@@ -15,7 +16,6 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 if getenv("AUTH_TYPE", None) == "auth":
-    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
