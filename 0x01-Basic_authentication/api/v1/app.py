57d74858acbea41f3d4736e01cf0dfd89df09041
VicvekSr <Olamideoso65@gmail.com>
2023-02-07 20:48:25 +0000

    add requests validator

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,7 +15,7 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-if getenv("AUTH_TYPE", None) == "auth":
+if getenv("AUTH_TYPE") == "auth":
     auth = Auth()
 
 
@@ -46,12 +46,11 @@ def before_request() -> str:
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+    if auth.require_auth(request.path, excluded_paths):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,8 +18,11 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ authorization header method """
-        if request is None or request.headers.get('Authorization', None):
+        if not request or not request.headers:
             return None
+        if not request.headers.get('Authorization'):
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current user method """
