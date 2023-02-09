b94db1d05a1c8532a6bdce545e856ff4f2a3aa47
VicvekSr <Olamideoso65@gmail.com>
2023-02-09 10:07:33 +0000

    add session cookies

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -51,13 +51,16 @@ def before_request() -> str:
         return
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, excluded_paths):
         if not auth.authorization_header(request):
             abort(401)
         if not auth.current_user(request):
             abort(403)
     request.current_user = auth.current_user(request)
+    if auth.authorization_header(request) and auth.session_cookie(request) == None:
+        abort(401)
 
 
 if __name__ == "__main__":
