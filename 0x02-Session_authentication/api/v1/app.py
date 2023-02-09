f96e7cd4bb0ff9fd88c2d0984a2ffe39f86cc51d
VicvekSr <Olamideoso65@gmail.com>
2023-02-09 10:15:48 +0000

    fix pycodestyle error

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -58,10 +58,10 @@ def before_request() -> str:
             abort(401)
         if not auth.current_user(request):
             abort(403)
+        if not auth.authorization_header(request)\
+                and not auth.session_cookie(request):
+            abort(401)
     request.current_user = auth.current_user(request)
-    if auth.authorization_header(request) is None and \
-       auth.session_cookie(request) is None:
-        abort(401)
 
 
 if __name__ == "__main__":
