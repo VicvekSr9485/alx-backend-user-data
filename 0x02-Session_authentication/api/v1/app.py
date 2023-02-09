df1b6caf996409092a7d238f27be66e365a42832
VicvekSr <Olamideoso65@gmail.com>
2023-02-09 23:19:59 +0000

    fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,25 +7,43 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
 auth = None
-if getenv("AUTH_TYPE") == "auth":
+if getenv("AUTH_TYPE", None) == "auth":
+    from api.v1.auth.auth import Auth
     auth = Auth()
-elif getenv("AUTH_TYPE") == "basic_auth":
+
+
+if getenv("AUTH_TYPE", None) == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif getenv("AUTH_TYPE") == "session_auth":
+
+elif getenv("AUTH_TYPE", None) == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
-else:
+elif getenv("AUTH_TYPE", None) == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden Error handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 @app.errorhandler(404)
@@ -35,37 +53,22 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ unauthorized error handler """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """ forbidden error handler """
-    return jsonify({"error": "Forbidden"}), 403
-
-
 @app.before_request
-def before_request() -> None:
-    """
-    Executed before each request
-    that is handled by a function
-    of that blueprint
-    """
-    excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
-            '/api/v1/auth_session/login/'
-            ]
-    if auth and auth.require_auth(request.path, excluded_paths):
-        if not auth.authorization_header(request)\
-                and not auth.session_cookie(request):
-            abort(401)
-        if not auth.current_user(request):
-            abort(403)
+def before_request() -> str:
+    """before request handler"""
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/auth_session/login/',
+                      '/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None and \
+       not auth.session_cookie(request):
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
     request.current_user = auth.current_user(request)
 
 
