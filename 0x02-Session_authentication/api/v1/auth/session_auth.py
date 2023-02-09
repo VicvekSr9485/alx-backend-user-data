dcaaa4a4444556f218135f4dbf4a1a1f257a8f95
VicvekSr <Olamideoso65@gmail.com>
2023-02-09 09:36:10 +0000

    fix pycodestyle error

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -8,6 +8,7 @@ import uuid
 class SessionAuth(Auth):
     """ Session authentication class """
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
         """ creates a Session ID for a user_id """
         if not user_id:
