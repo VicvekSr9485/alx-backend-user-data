2fb1efa96069e76385048c00dc96d03b73aea65e
VicvekSr <Olamideoso65@gmail.com>
2023-02-14 13:43:04 +0000

    destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,7 +79,7 @@ class Auth:
             return None
 
         try:
-            user = self_db.find_user_by(id=user_id)
+            user = self._db.find_user_by(id=user_id)
             self.update_user(user.id, session_id=None)
             return None
         except NoResultFound:
