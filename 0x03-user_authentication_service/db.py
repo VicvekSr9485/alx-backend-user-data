108056320f185feb7c1c571d39690169f83a1c94
VicvekSr <Olamideoso65@gmail.com>
2023-02-14 09:33:11 +0000

    updates user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -57,3 +57,13 @@ class DB:
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
+        """ updates user """
+        user = self.find_user_by(id=user_id)
+
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
+
+        self._session.commit()
