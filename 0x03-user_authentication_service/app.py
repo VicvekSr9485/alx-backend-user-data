045109ab4f80b3d2667a02532d56cfd9d9944bb3
VicvekSr <Olamideoso65@gmail.com>
2023-02-14 11:49:35 +0000

    Get session ID

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -32,5 +32,21 @@ def register_users() -> str:
         }), 400
 
 
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """  Log in """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    valid_credentials = AUTH.valid_login(email, password)
+    if not valid_credentials:
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": user.email, "message": "logged in"})
+    response = response.set_cookie("session_id", session_id)
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
