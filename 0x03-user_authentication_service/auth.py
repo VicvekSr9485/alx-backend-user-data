cab1989160d6bc9038833648173fdd6d5436150f
VicvekSr <Olamideoso65@gmail.com>
2023-02-14 10:12:32 +0000

    hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 """ auth provider """
 
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """ returns hashed password """
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(bytes, salt)
+    return hash
