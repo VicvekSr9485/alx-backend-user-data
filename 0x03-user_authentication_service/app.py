010ff157869947010c3581ddc5b6615173e09a7d
VicvekSr <Olamideoso65@gmail.com>
2023-02-14 11:51:17 +0000

    Get session ID

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Flask app """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 app = Flask(__name__)
 AUTH = Auth()
