c06884a5f3e26f416e50ab7e4f66231ba214902b
VicvekSr <Olamideoso65@gmail.com>
2023-02-15 07:37:33 +0000

    Update password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -88,7 +88,7 @@ def update_password() -> str:
     new_password = request.form.get("new_password")
 
     try:
-        AUTH.update_password(reset_token, password)
+        AUTH.update_password(reset_token, new_password)
     except Exception:
         abort(403)
     return jsonify({"email": email, "message": "Password updated"}), 200
