c83a2d4f49b2d3fe066bc6fc282016d0f799b7ba
VicvekSr <Olamideoso65@gmail.com>
2023-02-15 05:51:02 +0000

    add profile function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -59,5 +59,15 @@ def logout() -> str:
     return redirect("/")
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile() -> str:
+    """ User profile function """
+    cookie =  request.cookies.get("session_id", None)
+    user = AUTH.get_user_from_session_id(cookie)
+    if user is None or cookie is None:
+        abort(403)
+    return jsonify({"email": user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
