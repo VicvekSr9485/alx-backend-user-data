4b398fa72aa29f8e09e304d0bd57bd9be1881113
VicvekSr <Olamideoso65@gmail.com>
2023-02-19 12:51:14 +0000

    encrypt password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,6 +4,6 @@
 import bcrypt
 
 
-def hash_password(password) -> bytes:
+def hash_password(password: str) -> bytes:
     """ return hashed password """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
